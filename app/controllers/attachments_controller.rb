6aa41beb1a93248f3f17beb276869237d7ea9d22
Nikhil Hira <nikhil.hira@ilabsolutions.com>
2015-11-08 15:57:57 -0800

    Provide an 'Add another file' link that uses JavaScript so that users can upload more than one file

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,5 +1,14 @@
 class AttachmentsController < ApplicationController
 
+  skip_after_action :verify_authorized, only: [:new]
+
+  def new
+    @ticket = Ticket.new
+    @ticket.attachments.build
+    @index = params[:index].to_i
+    render layout: false
+  end
+
   def show
     attachment = Attachment.find(params[:id])
     authorize attachment, :show?
